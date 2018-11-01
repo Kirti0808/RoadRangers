c08a186d27f887ad770b1eb5e264f503b8847a7c
JAIDEEP DATTA <34120588+Jaideep-97@users.noreply.github.com>
2018-11-01 15:20:51 +0530

    commit16

--- autoquiz/autoquiz-qual.js
+++ autoquiz/autoquiz-qual.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Nov 1, 2018 15:06:00").getTime();
+var countDownDate = new Date("Nov 2, 2018 15:06:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
--- round3/auto-submit3.js
+++ round3/auto-submit3.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 31, 2018 01:24:00").getTime();
+var countDownDate = new Date("Nov 2, 2018 01:24:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
